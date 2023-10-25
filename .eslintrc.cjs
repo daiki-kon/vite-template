2caa0b2b14dd0678a0cbde9b8da433de7b5cb041
daiki-kon <61902151+daiki-kon@users.noreply.github.com>
2023-10-26 00:15:34 +0900

    :wrench:

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,24 +1,54 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true, "node": true, },
+  env: { browser: true, es2020: true, node: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
     "airbnb",
     "airbnb-typescript",
     "airbnb/hooks",
     "prettier",
+    "plugin:tailwindcss/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: [
+    "dist",
+    ".eslintrc.cjs",
+    ".eslintrc.*",
+    "vite.config.*",
+    "postcss.config.js",
+    "prettier.config.js",
+    "tailwind.config.js"
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+  },
+  plugins: ["react-refresh", "tailwindcss"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
     // Reactの明示的なインポートを不要にする
     "react/react-in-jsx-scope": "off",
+    "import/extensions": [
+      "error",
+      {
+        ignorePackages: true,
+        pattern: {
+          js: "never",
+          jsx: "never",
+          ts: "never",
+          tsx: "never",
+          json: "always",
+        },
+      },
+    ],
+    // スプレッド構文を許可
+    "react/jsx-props-no-spreading": "off",
+    "react/require-default-props": "off",
+    "import/prefer-default-export": "off",
+    "@typescript-eslint/no-shadow": "off",
   },
-}
+};
